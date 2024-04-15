72965a7c5b5a6e7ccb4a5bf6d8447638ca253cc9
Molkang <mitier26@gmail.com>
2024-04-16 00:50:35 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import logo from './logo.svg'
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-   return (
-      <div className="App">
-         <h1>Test</h1>
-         <h2>김혜린 추가</h2>
-         경록 테스트
-         <h2>우명균 추가</h2>
-         <h2>isus test2</h2>
-         <h2>나의 테스트</h2>
-         <h2>sol test</h2>
-      </div>
-   )
+    return (
+        <div className="App">
+            <h1>Test</h1>
+            <h2>김혜린 추가</h2>
+            경록 테스트
+            <h2>우명균 추가</h2>
+            <h2>isus test2</h2>
+            <h2>나의 테스트</h2>
+            <h2>sol test</h2>
+        </div>
+    );
 }
 
-export default App
+export default App;
