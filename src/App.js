23e0e34d075d0d9679feb6d15ffb3bf5c8a0c2f3
EUNSOL0313 <liat0313@gmail.com>
2024-04-15 22:44:38 +0900

    first test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-    return (
-        <div className="App">
-            <h1>Test</h1>
-        </div>
-    );
+   return (
+      <div className="App">
+         <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+               Edit <code>test-sol</code> and save to reload.
+            </p>
+            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+               Learn React
+            </a>
+         </header>
+      </div>
+   )
 }
 
-export default App;
+export default App
