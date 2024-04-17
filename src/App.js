052fb48276c1b8a9ad70f9256f0bff0d7c79adf7
Hyerin Kim <kdjsanh0217@naver.com>
2024-04-17 21:41:50 +0900

    로그인 후 유저 정보 userSlice에 업데이트시키기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import React, { useState } from 'react'
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import AppLayout from './Layout/AppLayout';
 import MainPage from './Pages/MainPage/MainPage';
 import LoginPage from './Pages/LoginPage/LoginPage';
@@ -13,11 +13,37 @@ import CampingDetailPage from './Pages/CampingDetailPage/CampingDetailPage';
 import NotFoundPage from './Pages/NotFoundPage/NotFoundPage';
 import SearchedPage from './Pages/SearchedPage/SearchedPage';
 import app from './firebase';
-import { getAuth } from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useDispatch } from 'react-redux';
+import userSlice, { clearUser} from './reducers/userSlice';
+
 
 function App() {
     const [LoginTrue, setLoginTrue] = useState(false);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
     const appAuth = getAuth(app); // firebase auth 인증서 
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(appAuth,(user)=>{// onAuth 인증된 사람만 이동되게
+          if(user){
+            navigate('/');
+            //userSlice Update 현재 유저로 상태 업데이트
+            dispatch(userSlice.actions.setUser({
+              uid: user.uid,
+              displayName: user.displayName,
+              photoURL : user.photoURL
+            }))
+          }else{
+            navigate('/auth/login');
+            dispatch(clearUser());
+          }
+        })
+        return () =>{
+          unsubscribe(); // Unsubscribe로 등록해준 부분을 없애주는 부분
+        }
+      },[appAuth])
+
 
     return (
         <>
