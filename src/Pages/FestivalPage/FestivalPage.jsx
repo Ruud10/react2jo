d09228d2dbe92898eb9269422f931e3697338b3b
kyeongrok <ghskdnwlsb7@gmail.com>
2024-04-19 22:53:50 +0900

    04192252

--- src/Pages/FestivalPage/FestivalPage.jsx
+++ src/Pages/FestivalPage/FestivalPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 const FestivalPage = () => {
   const [festivals, setFestivals] = useState([]);
@@ -7,20 +8,30 @@ const FestivalPage = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchFestivals = async () => {
+    const fetchData = async () => {
       try {
         const apiKey = "KQM2A5C9L5RvGwpDcXQv5wtEijp0j9vQIrFFSO2El1eZeSD3rnezIEKaWrg9hTRwrx4Zeg4V1Tguhm%2BmpTCTXA%3D%3D";
-        const url = `https://apis.data.go.kr/B551011/KorService1/locationBasedList1?numOfRows=10&pageNo=1&MobileOS=WIN&MobileApp=Festival&_type=json&listYN=N&arrange=A&mapX=127&mapY=36&radius=20000&contentTypeId=15&modifiedtime=20240418&serviceKey=${apiKey}`;
+        const numOfRows = 30;
+        const pageNo = 1;
+        const mobileOS = "WIN";
+        const mobileApp = "Festival";
+        const listYN = "Y";
+        const arrange = "A";
+        const mapX = "126";
+        const mapY = "37"; 
+        const radius = "90000000"; 
+        const contentTypeId = "15"; 
+        const modifiedtime = "20240418";
+        const url = `https://apis.data.go.kr/B551011/KorService1/locationBasedList1?numOfRows=${numOfRows}&pageNo=${pageNo}&MobileOS=${mobileOS}&MobileApp=${mobileApp}&_type=json&listYN=${listYN}&arrange=${arrange}&mapX=${mapX}&mapY=${mapY}&radius=${radius}&contentTypeId=${contentTypeId}&modifiedtime=${modifiedtime}&serviceKey=${apiKey}`;
 
         const response = await axios.get(url);
-
         const { data } = response;
         const { response: { body: { items: { item } } } } = data;
 
         console.log("Festivals data:", item);
 
-        if (item) {
-          setFestivals(Array.isArray(item) ? item : [item]);
+        if (Array.isArray(item) && item.length > 0) {
+          setFestivals(item);
         } else {
           setError("No festivals found");
         }
@@ -32,7 +43,7 @@ const FestivalPage = () => {
       }
     };
 
-    fetchFestivals();
+    fetchData();
   }, []);
 
   if (loading) {
@@ -43,16 +54,23 @@ const FestivalPage = () => {
     return <div>Error: {error}</div>;
   }
 
-
   return (
-    <div>
-      <h1>Festival List</h1>
-      <ul>
-        <li>
-          <pre>{JSON.stringify(festivals, null, 2)}</pre>
-        </li>
-      </ul>
-    </div>
+    <Container>ㅇ
+      <h1 className="my-4">전국 축제 리스트</h1>
+      <Row>
+        {festivals.map((festival, index) => (
+          <Col key={index} lg={4} md={6} className="mb-4">
+            <Card>
+              <Card.Body>
+                <Card.Title>{festival.title}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{festival.startDate}</Card.Subtitle>
+                <Card.Text>{festival.addr1}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
 
